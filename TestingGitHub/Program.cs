fcb584e25c7b6a698ea23d71645890ea8559376b
Anastasija Zuravliova <anas1719@edu.easj.dk>
2019-11-26 10:36:57 +0100

    Add project files.

---
+++ TestingGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
